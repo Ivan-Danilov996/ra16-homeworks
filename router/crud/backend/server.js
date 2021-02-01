0ccc1616164bea2c770aad8d46b9e002af677065
Ivan-Danilov996 <ivandanilov996@yandex.ru>
2021-02-02 00:27:36 +0300

    init commit

--- router/crud/backend/server.js
+++ router/crud/backend/server.js
@@ -7,7 +7,7 @@ const koaBody = require('koa-body');
 const app = new Koa();
 
 app.use(cors());
-app.use(koaBody({json: true}));
+app.use(koaBody({ json: true }));
 
 let posts = [];
 let nextId = 1;
@@ -15,23 +15,27 @@ let nextId = 1;
 const router = new Router();
 
 router.get('/posts', async (ctx, next) => {
+    console.log(posts)
     ctx.response.body = posts;
 });
 
-router.post('/posts', async(ctx, next) => {
-    const {id, content} = ctx.request.body;
+router.post('/posts', async (ctx, next) => {
+
+    const data = JSON.parse(ctx.request.body);
+
+    const { id, content } = data
 
     if (id !== 0) {
-        posts = posts.map(o => o.id !== id ? o : {...o, content: content});
+        posts = posts.map(o => o.id !== id ? o : { ...o, content: content });
         ctx.response.status = 204;
         return;
     }
 
-    posts.push({...ctx.request.body, id: nextId++, created: Date.now()});
+    posts.push({ ...data, id: nextId++, created: Date.now() });
     ctx.response.status = 204;
 });
 
-router.delete('/posts/:id', async(ctx, next) => {
+router.delete('/posts/:id', async (ctx, next) => {
     const postId = Number(ctx.params.id);
     const index = posts.findIndex(o => o.id === postId);
     if (index !== -1) {
